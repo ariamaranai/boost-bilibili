aae8c1eebb4593ca4e83e147ce65da881ca0ae9b
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-02-12 20:28:21 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-Object.freeze = a => a;
-Object.isFrozen =
-MediaSource.isTypeSupported = () => 1;
-Math.random = ()=> 0;
+Math.random = () => 0;
 {
+  let o = Object;
+  o.freeze = a => a;
+  o.isFrozen =
+  o.hasOwnProperty =
+  MediaSource.isTypeSupported = () => 1;
+
   let fet = fetch;
   fetch = (a, b) => a[2] != "d" && fet(a, b);
 }
