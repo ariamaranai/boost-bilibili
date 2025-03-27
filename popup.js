83fbf77a3796ebbb2492383a695580140a9ddfe1
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-03-27 10:06:08 +0900

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,1 +1,1 @@
-onkeyup = e => (e.key == "Enter" && close(chrome.tabs.update({ url: "https://search.bilibili.com/all?keyword=" + e.target.value })));
+onkeyup = e => e.keyCode == 13 && close(chrome.tabs.update({ url: "https://search.bilibili.com/all?keyword=" + e.target.value }));
