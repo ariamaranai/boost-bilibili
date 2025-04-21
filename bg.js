fceee9b0ba97973d689b9ef13352934ed19dff3c
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-04-21 17:08:43 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -10,4 +10,10 @@ chrome.omnibox.onInputEntered.addListener(q =>
   chrome.tabs.update({ url: "https://search.bilibili.com/all?keyword=" + q })
 );
 chrome.action.setPopup({ popup: "popup.htm" });
-chrome.contextMenus.onClicked.addListener(() => chrome.action.openPopup());
+chrome.contextMenus.onClicked.addListener((_, { windowId }) =>
+  chrome.windows.get(windowId, async window => (
+    window.state == "fullscreen" &&
+    await chrome.windows.update(windowId, { state: "maximized" }),
+    chrome.action.openPopup()
+  ))
+);
