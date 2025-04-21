67fb00b167981b41423a3a55b08aa5ac122e4a28
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-04-21 17:11:42 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -7,13 +7,19 @@ chrome.runtime.onInstalled.addListener(() =>
   })
 );
 chrome.omnibox.onInputEntered.addListener(q =>
-  chrome.tabs.update({ url: "https://search.bilibili.com/all?keyword=" + q })
+  chrome.tabs.update({
+    url: "https://search.bilibili.com/all?keyword=" + q
+  })
 );
-chrome.action.setPopup({ popup: "popup.htm" });
+chrome.action.setPopup({
+  popup: "popup.htm"
+});
 chrome.contextMenus.onClicked.addListener((_, { windowId }) =>
   chrome.windows.get(windowId, async window => (
     window.state == "fullscreen" &&
-    await chrome.windows.update(windowId, { state: "maximized" }),
+    await chrome.windows.update(windowId, {
+      state: "maximized"
+    }),
     chrome.action.openPopup()
   ))
 );
--- popup.js
+++ popup.js
@@ -1,1 +1,3 @@
-onkeyup = e => e.keyCode == 13 && close(chrome.tabs.update({ url: "https://search.bilibili.com/all?keyword=" + e.target.value }));
+onkeyup = e => e.keyCode == 13 && close(chrome.tabs.update({
+  url: "https://search.bilibili.com/all?keyword=" + e.target.value
+}));
